ac1aa9908d23619bab8849b32ba51d88e44161dc
Adam Tang <adamtang@gmail.com>
2022-12-23 21:11:55 -0800

    merged user_details table back into users table
    removed model and seed files for user_details

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
 from .db import db
 from .user import User
-from .user_detail import UserDetail
 from .song import Song
 from .comment import Comment
 from .playlist import Playlist
--- app/models/user.py
+++ app/models/user.py
@@ -3,57 +3,59 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from .song_like import song_likes
 from .playlist_like import playlist_likes
-from .user_detail import UserDetail
 from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+  __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(255), nullable=False, unique=True)
+  hashed_password = db.Column(db.String(255), nullable=False)
+  display_name = db.Column(db.String(50))
+  avatar_url = db.Column(db.String)
+  banner_url = db.Column(db.String)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+  updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
-    user_detail = db.relationship(
-        "UserDetail", back_populates="user", uselist=False)
-    songs = db.relationship("Song", back_populates="user")
-    playlists = db.relationship("Playlist", back_populates="user")
-    comments = db.relationship("Comment", back_populates="user")
+  songs = db.relationship("Song", back_populates="user")
+  playlists = db.relationship("Playlist", back_populates="user")
+  comments = db.relationship("Comment", back_populates="user")
 
-    pl_likes = db.relationship(
-        "Playlist",
-        secondary=playlist_likes,
-        back_populates="pl_likes"
-    )
+  pl_likes = db.relationship(
+    "Playlist",
+    secondary=playlist_likes,
+    back_populates="pl_likes"
+  )
 
-    likes = db.relationship(
-        "Song",
-        secondary=song_likes,
-        back_populates="likes"
-    )
+  likes = db.relationship(
+    "Song",
+    secondary=song_likes,
+    back_populates="likes"
+  )
 
-    @property
-    def password(self):
-        return self.hashed_password
+  @property
+  def password(self):
+    return self.hashed_password
 
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+  @password.setter
+  def password(self, password):
+    self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
 
-    def to_dict(self):
-        user_detail = UserDetail.query.filter(UserDetail.user_id == self.id).first()
+  def to_dict(self):
+    user_detail = UserDetail.query.filter(UserDetail.user_id == self.id).first()
 
-        return {
-            'id': self.id,
-            'email': self.email,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at,
-            "user_detail": user_detail.to_dict(),
-            "comment_amount":len(self.comments),
-            "like_amount":len(self.likes)
-        }
+    return {
+      'id': self.id,
+      'email': self.email,
+      'display_name': self.display_name,
+      'avatar_url': self.avatar_url,
+      'banner_url': self.banner_url
+      'created_at': self.created_at,
+      'updated_at': self.updated_at,
+      "comment_amount":len(self.comments),
+      "like_amount":len(self.likes)
+    }
