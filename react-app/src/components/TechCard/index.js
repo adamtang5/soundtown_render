437a223f20220ea5f17aad21bbd4b60079e468ea
Adam Tang <adamtang@gmail.com>
2023-01-09 12:24:39 -0800

    simplified TechCard

--- react-app/src/components/TechCard/index.js
+++ react-app/src/components/TechCard/index.js
@@ -2,7 +2,7 @@ const TechCard = ({ tech }) => {
   return (
     <div className="tech-card flex-row">
       <div className="tech-icon">
-        <img src={tech?.image_url} alt={tech?.name} />
+        <img src={tech.image_url} alt={tech.name} />
       </div>
     </div>
   );
