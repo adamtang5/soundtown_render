d3dbbf1fdf01476d99c81b5b4affbeb8051b82fb
Adam Tang <adamtang@gmail.com>
2023-01-09 00:35:57 -0800

    implemented spinning pinwheel for loading sidebar

--- react-app/src/components/CreatorCard/index.js
+++ react-app/src/components/CreatorCard/index.js
@@ -1,31 +1,14 @@
-import { useEffect, useState } from "react";
 import CreatorAvatar from "./CreatorAvatar";
 import "./CreatorCard.css";
 
 const CreatorCard = ({ creator }) => {
-  const [updatedCreator, setUpdatedCreator] = useState(null);
-  const [creatorLoaded, setCreatorLoaded] = useState(false);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${creator.github_username}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setUpdatedCreator({
-          ...creator,
-          avatar_url: data.avatar_url,
-          github_url: data.html_url,
-        });
-      })
-
-      .then(() => setCreatorLoaded(true));
-  }, []);
-
+  if (!creator?.avatar_url) return null;
   return (
-    <a href={updatedCreator?.github_url} target="_blank" rel="noreferrer">
+    <a href={creator.github_url} target="_blank" rel="noreferrer">
       <div className="creator-card flex-column">
-        {creatorLoaded && <CreatorAvatar creator={updatedCreator} />}
+        <CreatorAvatar creator={creator} />
         <div className="creator-name-tag">
-          {`${updatedCreator?.first_name} ${updatedCreator?.last_name}`}
+          {`${creator.first_name} ${creator.last_name}`}
         </div>
       </div>
     </a>
--- react-app/src/components/HomePage/Sidebar.js
+++ react-app/src/components/HomePage/Sidebar.js
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CreatorCard from "../CreatorCard";
 import creators from '../CreatorCard/creators.json';
 import TechCard from "../TechCard";
 import techs from '../TechCard/techs.json';
+import { ImSpinner3 } from 'react-icons/im';
+
+const updateCreator = async (creator) => {
+  const res = await fetch(`https://api.github.com/users/${creator.github_username}`);
+  const data = await res.json();
+  return {
+    ...creator,
+    avatar_url: data.avatar_url,
+    github_url: data.html_url,
+  };
+};
 
 const Sidebar = () => {
+  useEffect(() => {
+    for (let i = 0; i < creators.length; i++) {
+      updateCreator(creators[i])
+        .then(newData => creators[i] = newData);
+    }
+  }, []);
+
   return (
     <div className="sidebar_container flex-column">
-      <div className="tech-icon">
-        <a href="https://github.com/stevenkleinberg/Python_Group7_Soundcloud" target="_blank" rel="noreferrer">
-          <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
-        </a>
-      </div>
-      <h3 className="sidebar-text">Creators</h3>
-      {creators?.map(creator => (
-        <CreatorCard key={creator.github_username} creator={creator} />
-      ))}
-      <h3 className="sidebar-text">Built with</h3>
-      <div className="techs flex-row">
-        {techs?.map(tech => (
-          <TechCard key={tech.name} tech={tech} />
-        ))}
-      </div>
+      {creators.every(detail => detail.avatar_url) ?
+        (
+          <>
+            <div className="tech-icon">
+              <a href="https://github.com/adamtang5/soundtown_render" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
+              </a>
+            </div>
+            <h3 className="sidebar-text">Creators</h3>
+            {creators?.map(creator => (
+              <CreatorCard
+                key={creator.github_username}
+                creator={creator}
+              />
+            ))}
+            <h3 className="sidebar-text">Built with</h3>
+            <div className="techs flex-row">
+              {techs?.map(tech => (
+                <TechCard key={tech.name} tech={tech} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <ImSpinner3 className="spinning pinwheel" />
+        )}
     </div>
-  )
+  );
 };
 
 export default Sidebar;
