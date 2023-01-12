a712bfc666e3db06eec3936cd4e8e1a9bf2331cd
Adam Tang <adamtang@gmail.com>
2023-01-12 00:10:06 -0800

    sorted routes by order of presentation to user

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { authenticate } from "./store/session";
-import UploadSong from "./components/SongFolders/UploadSongForm";
-import HomePage from "./components/HomePage";
-import EditSongForm from "./components/SongFolders/EditSongForm";
+import { getAllUsers } from "./store/user";
 import { getAllSongs } from "./store/song";
 import { getAllPlaylists } from "./store/playlist";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { ModalProvider } from "./components/Context/Modal";
+import NavBar from "./components/NavBar";
+import SplashPage from "./components/SplashPage";
+import HomePage from "./components/HomePage";
+import ExplorePage from "./components/ExplorePage";
+import LibraryPage from "./components/LibraryPage";
+import UploadSong from "./components/SongFolders/UploadSongForm";
+import EditSongForm from "./components/SongFolders/EditSongForm";
 import SingleSong from "./components/SongFolders/SingleSong";
 import Audio from "./components/AudioPlayer/Audio";
-import SplashPage from "./components/SplashPage";
 import PlaylistsPage from "./components/PlaylistFolders/PlaylistsPage";
-import LibraryPage from "./components/LibraryPage";
-import { ModalProvider } from "./components/Context/Modal";
-import { getAllUsers } from "./store/user";
 import NewUsersPage from "./components/NewUserPage";
-import ExplorePage from "./components/ExplorePage";
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
@@ -57,6 +57,9 @@ const App = () => {
           <ProtectedRoute path="/explore">
             <ExplorePage />
           </ProtectedRoute>
+          <ProtectedRoute path="/library">
+            <LibraryPage />
+          </ProtectedRoute>
           <Route path="/upload-song" exact={true}>
             <UploadSong />
           </Route>
@@ -72,9 +75,6 @@ const App = () => {
           </ProtectedRoute>
           <ProtectedRoute path="/playlists/:id" exact={true}>
             <PlaylistsPage />
-          </ProtectedRoute>
-          <ProtectedRoute path="/library">
-            <LibraryPage />
           </ProtectedRoute>
           <Route>
             <p>not found</p>
