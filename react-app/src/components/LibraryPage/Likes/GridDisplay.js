f2fa8f761769ac2c971a1e484a3b6831fbcffc6c
Adam Tang <adamtang@gmail.com>
2023-01-11 23:27:58 -0800

    refactored some components in LibraryPage

--- react-app/src/components/LibraryPage/Likes/GridDisplay.js
+++ react-app/src/components/LibraryPage/Likes/GridDisplay.js
@@ -3,11 +3,11 @@ import SongTile from "../../GalleryCard/SongTile";
 
 const GridDisplay = ({ likedSongs }) => {
   return (
-    <div className="flex-row liked_songs_container">
+    <article className="flex-row liked_songs_container">
       {likedSongs.map((song) => (
         <SongTile song={song} key={song.id} />
       ))}
-    </div>
+    </article>
   );
 };
 
