40ce7716a91262c5fb74ed1d19df15a198a4925e
Adam Tang <adamtang@gmail.com>
2023-01-24 11:37:17 -0800

    refactored button group on SingleSong page

---
+++ react-app/src/components/Buttons/DropdownButton.js
@@ -0,0 +1,34 @@
+const DropdownButton = ({
+  toggleLabel,
+  toggleClasses,
+  showDropdown,
+  setShowDropdown,
+  dropdownUlClasses,
+  dropdownItems,
+  dropdownButtonClasses,
+}) => {
+  return (
+    <div style={{ position: "relative" }}>
+      <button
+        onClick={() => setShowDropdown(true)}
+        className={toggleClasses.join(' ')}
+      >{toggleLabel}</button>
+      {showDropdown && (
+        <ul
+          className={dropdownUlClasses.join(' ')}
+          style={{ position: "absolute" }}
+        >
+          {dropdownItems.map(item => (
+            <li
+              key={item.label}
+              className="flex-row cursor-pointer"
+              onClick={item.onClick}
+            >{item.label}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DropdownButton;
