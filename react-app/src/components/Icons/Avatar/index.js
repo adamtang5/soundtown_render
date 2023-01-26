da0718f52740939e1963ad34fda1fe130d045899
Adam Tang <adamtang@gmail.com>
2023-01-26 09:21:17 -0800

    refactored Avatar

--- react-app/src/components/Icons/Avatar/index.js
+++ react-app/src/components/Icons/Avatar/index.js
@@ -1,28 +1,24 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Avatar.css";
 
-const Avatar = ({ user, isNotLink }) => {
+const Avatar = ({ user, isLink }) => {
   const Visual = () => {
-    return (
-      <div className="avatar">
-        {user?.avatar_url ? (
-          <img src={user?.avatar_url} alt={user?.display_name} />
-        ) : (
-          <div className="avatar-placeholder" />
-        )}
-      </div>
+    return user?.avatar_url ? (
+      <img src={user?.avatar_url} alt={user?.display_name} />
+    ) : (
+      <div className="avatar-placeholder" />
     );
   };
 
-  if (isNotLink) {
-    return <Visual />;
-  } else {
-    return (
-      <NavLink to={`/users/${user?.id}`}>
-        <Visual />
-      </NavLink>
-    );
-  }
+  return isLink ? (
+    <Link to={`/users/${user?.id}`} className="avatar">
+      <Visual />
+    </Link>
+  ) : (
+    <div className="avatar">
+      <Visual />
+    </div>
+  )
 };
 
 export default Avatar;
