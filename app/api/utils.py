9f19e6d77cc0d2356d2d4bc91994878a56d407dd
Adam Tang <adamtang@gmail.com>
2023-01-28 14:30:34 -0800

    simplified API routes for song and user
    testing upload and edit song and user flows

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,9 +1,12 @@
+FILE_TYPE_ERROR = {"errors": ["file type not permitted"]}, 400
+UNAUTHORIZED_ERROR = {"errors": ["unauthorized"]}, 403
+
 def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
-    return errorMessages
+  """
+  Simple function that turns the WTForms validation errors into a simple list
+  """
+  errorMessages = []
+  for field in validation_errors:
+    for error in validation_errors[field]:
+      errorMessages.append(f'{field} : {error}')
+  return errorMessages
