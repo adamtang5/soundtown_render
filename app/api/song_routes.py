1c870cae32fe19371e6c3e99022ace82aef5aedf
Adam Tang <adamtang@gmail.com>
2023-01-28 20:47:19 -0800

    removed comments

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -7,7 +7,8 @@ from app.api.utils import (
   validation_errors_to_error_messages, FILE_TYPE_ERROR, UNAUTHORIZED_ERROR
 )
 from app.s3_helpers import (
-  upload_file_to_s3, allowed_file, get_unique_filename)
+  upload_file_to_s3, allowed_file, get_unique_filename
+)
 from sqlalchemy.orm import relationship, sessionmaker, joinedload
 
 song_routes = Blueprint('song', __name__)
@@ -40,29 +41,6 @@ def new_song():
     db.session.add(song)
     db.session.commit()
     return song.to_dict()
-    # keys = list(request.files.to_dict().keys())
-    # if len(keys) != 2:
-    #   return jsonify({"error": "Missing file(s)"}), 400
-    # raw_audio_url = request.files["audio_url"]
-    # print("---------------audio url----------------", raw_audio_url)
-    # raw_image_url = request.files["image_url"]
-    # print("---------------image url----------------", raw_image_url)
-
-    # if not allowed_file(raw_audio_url.filename):
-    #   return {"errors": ["audio file type not permitted"]}
-
-    # if not allowed_file(raw_image_url.filename):
-    #   return {"errors": ["image file type not permitted"]}
-
-    # raw_audio_url.filename = get_unique_filename(raw_audio_url.filename)
-    # raw_image_url.filename = get_unique_filename(raw_image_url.filename)
-
-    # audio_upload = upload_file_to_s3(raw_audio_url)
-    # image_upload = upload_file_to_s3(raw_image_url)
-
-    # audio_url = audio_upload["url"]
-    # image_url = image_upload["url"]
-
   else:
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
@@ -110,76 +88,9 @@ def edit_song(id):
     song.updated_at = datetime.now()
     db.session.commit()
     return song.to_dict()
-
-    # if not any(request.files):
-    #   song = Song.query.get(int(request.form["id"]))
-    # else:
-    #   keys = list(request.files.to_dict().keys())
-    #   if len(keys) == 2:
-    #     raw_audio_url = request.files["audio_url"]
-    #     raw_image_url = request.files["image_url"]
-
-    #     if not allowed_file(raw_audio_url.filename):
-    #       return {"errors": ["audio file type not permitted"]}
-
-    #     if not allowed_file(raw_image_url.filename):
-    #       return {"errors": ["image file type not permitted"]}
-
-    #     raw_audio_url.filename = get_unique_filename(
-    #       raw_audio_url.filename)
-    #     raw_image_url.filename = get_unique_filename(
-    #       raw_image_url.filename)
-
-    #     audio_upload = upload_file_to_s3(raw_audio_url)
-    #     image_upload = upload_file_to_s3(raw_image_url)
-
-    #     audio_url = audio_upload["url"]
-    #     image_url = image_upload["url"]
-
-    #     song = Song.query.get(int(request.form["id"]))
-    #     song.title = request.form['title']
-    #     song.audio_url = audio_url,
-    #     song.description = request.form['description']
-    #     song.image_url = image_url
-    #     song.updated_at = datetime.now()
-    #   elif keys[0] == "audio_url":
-    #     raw_audio_url = request.files["audio_url"]
-
-    #     if not allowed_file(raw_audio_url.filename):
-    #       return {"errors": ["audio file type not permitted"]}
-
-    #     raw_audio_url.filename = get_unique_filename(
-    #       raw_audio_url.filename)
-
-    #     audio_upload = upload_file_to_s3(raw_audio_url)
-
-    #     audio_url = audio_upload["url"]
-
-    #     song = Song.query.get(int(request.form["id"]))
-    #     song.title = request.form['title']
-    #     song.audio_url = audio_url,
-    #     song.description = request.form['description']
-    #     song.updated_at = datetime.now()
-    #   elif keys[0] == "image_url":
-    #     raw_image_url = request.files["image_url"]
-
-    #     if not allowed_file(raw_image_url.filename):
-    #       return {"errors": ["image file type not permitted"]}
-
-    #     raw_image_url.filename = get_unique_filename(
-    #       raw_image_url.filename)
-
-    #     image_upload = upload_file_to_s3(raw_image_url)
-
-    #     image_url = image_upload["url"]
-
-    #     song = Song.query.get(int(request.form["id"]))
-    #     song.title = request.form['title']
-    #     song.description = request.form['description']
-    #     song.image_url = image_url
-    #     song.updated_at = datetime.now()
   else:
     return {'errors': validation_errors_to_error_messages(form.errors)}
+
 
 # GET /api/songs/
 @song_routes.route('/')
