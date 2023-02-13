99f4161f9c93121e32aaf202b635c5fa749e2032
Adam Tang <adamtang@gmail.com>
2023-02-12 23:37:25 -0800

    refactoring CreatePlaylistAddSongs

--- react-app/src/components/PlaylistFolders/AddToPlaylist/index.js
+++ react-app/src/components/PlaylistFolders/AddToPlaylist/index.js
@@ -71,7 +71,7 @@ const AddToPlaylist = ({ song }) => {
           handleEnlist={handleEnlist}
         />
       ) : (
-        <CreatePlaylistAddSong songsArr={[song?.id]} />
+        <CreatePlaylistAddSong song={song} />
       )}
     </>
   );
