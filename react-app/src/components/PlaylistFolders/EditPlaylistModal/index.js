20cf2cea21f14cdbf3013c7931dc1bd00024c121
Adam Tang <adamtang@gmail.com>
2023-03-23 14:14:40 -0700

    corrected structure of EditPlaylistForm

--- react-app/src/components/PlaylistFolders/EditPlaylistModal/index.js
+++ react-app/src/components/PlaylistFolders/EditPlaylistModal/index.js
@@ -1,50 +1,44 @@
 import React, { useState } from "react";
-
-import "./AddEditPlaylist.css";
-import EditPlaylistForm from "../EditPlaylist";
+import EditPlaylistForm from "../EditPlaylistForm";
 import AllTracksModal from "./AllTracksModal";
-import NewPlaylistForm from "../CreatePlaylist";
+import "./AddEditPlaylist.css";
 
-const EditPlaylistModal = ({ modalMode, modalFunction, songArr }) => {
+const EditPlaylistModal = ({ setShowModal, songArr }) => {
   const [mode, setMode] = useState("basic");
-  return (
-    <div className="AEP_modal">
-      <div className="flex-column AEP_modal_inner_container">
-        <div className="flex-row AEP_inner_container_titles">
-          <p
-            onClick={() => setMode("basic")}
-            className={
-              mode === "basic" ? "AEP_basic_title selected" : "AEP_basic_title"
-            }
-          >
-            Basic Info
-          </p>
 
-          {modalMode === "edit" && (
-            <p
-              onClick={() => setMode("tracks")}
-              className={
-                mode === "tracks"
-                  ? "AEP_basic_title selected"
-                  : "AEP_basic_title"
-              }
-            >
-              Tracks
-            </p>
-          )}
-        </div>
-        {modalMode === "edit" ? (
-          <>
-            {mode === "basic" && (
-              <EditPlaylistForm modalFunction={modalFunction} />
-            )}
-            {mode === "tracks" && <AllTracksModal songArr={songArr} />}
-          </>
-        ) : (
-          <NewPlaylistForm modalFunction={modalFunction} />
-        )}
-      </div>
-    </div>
+  const navData = [
+    {
+      mode: 'basic',
+      label: 'Basic info',
+    },
+    {
+      mode: 'tracks',
+      label: 'Tracks',
+    },
+  ];
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="nav-header flex-row">
+            {navData.map((data, idx) => (
+              <li
+                key={idx}
+                onClick={() => setMode(data.mode)}
+                className={mode === data.mode ? "active" : "cursor-pointer"}
+              >{data.label}</li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
+      {mode === "basic" ? (
+        <EditPlaylistForm setShowModal={setShowModal} />
+      ) : (
+        <AllTracksModal songArr={songArr} />
+      )}
+    </>
   );
 };
 
