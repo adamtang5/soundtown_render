03753fcd3edbcd277d54fd3fd54e9cadd9ac4088
Adam Tang <adamtang@gmail.com>
2023-03-28 22:43:08 -0700

    moved UploadSongModal into UploadButton

--- react-app/src/components/NavBar/NavBarRight.js
+++ react-app/src/components/NavBar/NavBarRight.js
@@ -1,10 +1,13 @@
-import UserProfile from "./UserProfile";
-import UploadButton from "./UploadButton";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Modal } from "../Context/Modal";
+import UploadButton from "./UploadButton";
+import UploadSongForm from "../SongFolders/UploadSongForm";
+import UserProfile from "./UserProfile";
 
-
-const NavBarRight = ({ showUploadModal, setShowUploadModal }) => {
+const NavBarRight = () => {
   const sessionUser = useSelector(state => state.session.user);
+  const [showUploadModal, setShowUploadModal] = useState(false);
 
   return (
     <div className="navbar-right">
@@ -12,6 +15,15 @@ const NavBarRight = ({ showUploadModal, setShowUploadModal }) => {
         showUploadModal={showUploadModal}
         setShowUploadModal={setShowUploadModal}
       />
+      {showUploadModal && (
+        <Modal
+          onClose={() => setShowUploadModal(false)}
+          position="center"
+          paddingTop={0}
+        >
+          <UploadSongForm setShowUploadModal={setShowUploadModal} />
+        </Modal>
+      )}
       <div className="user-navdrop-div">
         <UserProfile user={sessionUser} />
       </div>
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,30 +1,13 @@
-import React, { useState } from "react";
-import { Modal } from "../Context/Modal";
 import NavBarLeft from "./NavBarLeft";
-import UploadSongForm from "../SongFolders/UploadSongForm";
-import "./NavBar.css";
 import NavBarRight from "./NavBarRight";
+import "./NavBar.css";
 
 const NavBar = () => {
-  const [showUploadModal, setShowUploadModal] = useState(false);
-
   return (
     <header className="navbar-bg">
       <div className="navbar flex-row">
         <NavBarLeft />
-        <NavBarRight
-          showUploadModal={showUploadModal}
-          setShowUploadModal={setShowUploadModal}
-        />
-        {showUploadModal && (
-          <Modal
-            onClose={() => setShowUploadModal(false)}
-            position="center"
-            paddingTop={0}
-          >
-            <UploadSongForm setShowUploadModal={setShowUploadModal} />
-          </Modal>
-        )}
+        <NavBarRight />
       </div>
     </header>
   );
