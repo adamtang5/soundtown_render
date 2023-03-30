89d6c102d088d310bb5f3433ec76fbdfe4384f4a
Adam Tang <adamtang@gmail.com>
2023-03-29 23:43:40 -0700

    refactoring components on AuthForm

--- react-app/src/modals/SignUpForm.js
+++ react-app/src/modals/SignUpForm.js
@@ -55,90 +55,89 @@ const SignUpForm = ({ setShowSignUpModal, setShowLoginModal }) => {
   }
 
   return (
-    <div className="flex-row">
-      <form className="signup_form flex-column " onSubmit={onSignUp}>
-        <button
-          className="login_form_btn cursor-pointer or-shdw-hov"
-          type="submit"
-          onClick={demoLogin}
-        >
-          <FaUserAlt /> Continue with Demo User
+    <form className="flex-column" onSubmit={onSignUp}>
+      <button
+        className="login_form_btn cursor-pointer or-shdw-hov"
+        type="submit"
+        onClick={demoLogin}
+      >
+        <FaUserAlt /> Continue with Demo User
+      </button>
+      <button
+        className="login_form_btn cursor-pointer toggle-sign-up or-shdw-hov "
+        type="submit"
+        onClick={toggleModals}
+      >
+        <IoLogOutSharp className="proF-icon" /> Continue to Log In
+      </button>
+
+      <div className="half-Div">
+        <div className="half-line"></div>
+        <h3>or</h3>
+        <div className="half-line"></div>
+      </div>
+
+      <div className="form_field">
+        <input
+          placeholder="email"
+          style={{ marginBottom: '10px' }}
+
+          className="field"
+          type="text"
+          name="email"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </div>
+      <div className="form_field">
+        <input
+          placeholder="password"
+          style={{ marginBottom: '10px' }}
+
+          className="field"
+          type="password"
+          name="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </div>
+      <div className="form_field">
+        <input
+          className="field"
+
+          type="password"
+          name="repeat_password"
+          onChange={e => setRepeatPassword(e.target.value)}
+          value={repeatPassword}
+          required={true}
+          placeholder="confirm password"
+        />
+      </div>
+
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind} className="error-text">{error}</div>
+        ))}
+      </div>
+
+      <div className="form_button flex-row">
+        <button className="login_form_btn cursor-pointer cnt-login or-shdw-hov" type="submit">
+          Accept {'&'} Continue
         </button>
-        <button
-          className="login_form_btn cursor-pointer toggle-sign-up or-shdw-hov "
-          type="submit"
-          onClick={toggleModals}
-        >
-          <IoLogOutSharp className="proF-icon" /> Continue to Log In
-        </button>
+      </div>
 
-        <div className="half-Div">
-          <div className="half-line"></div>
-          <h3>or</h3>
-          <div className="half-line"></div>
-        </div>
-
-        <div className="form_field">
-          <input
-            placeholder="email"
-            style={{ marginBottom: '10px' }}
-
-            className="field"
-            type="text"
-            name="email"
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-        </div>
-        <div className="form_field">
-          <input
-            placeholder="password"
-            style={{ marginBottom: '10px' }}
-
-            className="field"
-            type="password"
-            name="password"
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-        </div>
-        <div className="form_field">
-          <input
-            className="field"
-
-            type="password"
-            name="repeat_password"
-            onChange={e => setRepeatPassword(e.target.value)}
-            value={repeatPassword}
-            required={true}
-            placeholder="confirm password"
-          />
-        </div>
-
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind} className="error-text">{error}</div>
-          ))}
-        </div>
-
-        <div className="form_button flex-row">
-          <button className="login_form_btn cursor-pointer cnt-login or-shdw-hov" type="submit">
-            Accept {'&'} Continue
-          </button>
-        </div>
-        <div className="priv-tag-div">
-          <p className="priv-tag">
-            When registering, you agree that we will not use your provided
-            data from the registration and we won't send you notifications on
-            our products and services. You can not unsubscribe from our non-existent
-            notifications at this time in your settings. For non-existent additional
-            info please do not refer to our fake <span style={{ color: 'blue', cursor: 'not-allowed' }}>Privacy Policy</span>.
-          </p>
-        </div>
-      </form>
-    </div>
+      <div className="priv-tag-div">
+        <p className="priv-tag">
+          When registering, you agree that we will not use your provided
+          data from the registration and we won't send you notifications on
+          our products and services. You can not unsubscribe from our non-existent
+          notifications at this time in your settings. For non-existent additional
+          info please do not refer to our fake <span style={{ color: 'blue', cursor: 'not-allowed' }}>Privacy Policy</span>.
+        </p>
+      </div>
+    </form>
   );
 };
 
