9b3c28e5f695404287d6820a7de0c42d92bf99b2
Adam Tang <adamtang@gmail.com>
2023-04-13 21:01:24 -0700

    absorbed SongTileActions into SongTile

--- react-app/src/components/GalleryCard/SongTile.js
+++ react-app/src/components/GalleryCard/SongTile.js
@@ -1,11 +1,104 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import SongTileActions from "./SongTileActions";
+import { NavLink } from "react-router-dom";
+import { Modal } from "../Context/Modal";
 import { getAllUsers } from "../../store/user";
-import { loadSong } from "../../store/player";
+import { loadSong, queueSong } from "../../store/player";
 import { likeSong, unlikeSong } from "../../store/song";
+import AddSongToPlaylist from "../../modals/AddSongToPlaylist";
+import { ImMenu3 } from "react-icons/im";
+import { CgPlayList } from "react-icons/cg";
+import { MdOutlinePlaylistAdd } from "react-icons/md";
 import "./Tile.css";
+
+const Actions = ({ song }) => {
+  const playingId = useSelector(state => state.player.playingId);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const [showPlaylistModal, setShowPlaylistModal] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  const openPlaylistModal = () => {
+    if (showPlaylistModal) return;
+    setShowPlaylistModal(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const addSongToQueue = (id) => {
+    if (!playingId) {
+      dispatch(loadSong(id));
+    } else {
+      dispatch(queueSong(id));
+    }
+  };
+
+  const dropdownItems = [
+    {
+      onClick: () => addSongToQueue(song.id),
+      leftIcon: <CgPlayList className="cg-icon" />,
+      label: "Add to Queue",
+    },
+    {
+      onClick: openPlaylistModal,
+      leftIcon: <MdOutlinePlaylistAdd className="cg-icon" />,
+      label: "Add to Playlist",
+    },
+  ];
+
+  return (
+    <div className="overlay-actions">
+      <div className="dropdown-hamburger" onClick={openMenu}>
+        <ImMenu3 className="hamburger-icon cursor-pointer" />
+      </div>
+      <div className="dropdown-container">
+        {showMenu && (
+          <ul className="dropdown-list flex-column">
+            {dropdownItems.map((item, idx) => (
+              <li
+                key={idx}
+                className="flex-row cursor-pointer"
+                onClick={item.onClick}
+              >
+                {item.leftIcon}{item.label}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {
+        showPlaylistModal && (
+          <Modal
+            onClose={() => setShowPlaylistModal(false)}
+            position="top"
+            paddingTop={50}
+          >
+            <div className="playlist-modal-container">
+              <AddSongToPlaylist
+                song={song}
+                setShowModal={setShowPlaylistModal}
+              />
+            </div>
+          </Modal>
+        )
+      }
+    </div>
+  );
+};
 
 const SongTile = ({ song, setShowModal }) => {
   const dispatch = useDispatch();
@@ -51,7 +144,7 @@ const SongTile = ({ song, setShowModal }) => {
       >
         {sessionUser != null ? (
           <div className="overlay-group flex-row">
-            <SongTileActions song={song} />
+            <Actions song={song} />
             <div onClick={handlePlay} className="overlay-play">&#9654;</div>
             <div className="overlay-like">
               {song?.likes?.includes(sessionUser.id) && (
