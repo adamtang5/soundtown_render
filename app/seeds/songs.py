8a3852e63d4570ff9892fb8d4d121f85a8b3410d
Adam Tang <adamtang@gmail.com>
2023-12-13 00:49:42 -0800

    simplified songs seeds

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -7,13 +7,12 @@ from app.models import db, Song, User
 
 def seed_songs():
   user_ids = [user.id for user in User.query.all()]
-  num_users = len(User.query.all())
   with open(os.getcwd()+"/app/seeds/songs.json") as f:
     data = json.load(f)
 
     for song_dict in data['songs']:
       new_song = Song(
-        user_id=user_ids[random.randint(0, num_users-1)],
+        user_id=random.choice(user_ids),
         title=song_dict["title"],
         audio_url=song_dict["audio_url"],
         description=song_dict["description"],
