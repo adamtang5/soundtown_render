23118a0c64ab10b6efbb94c41a84eff328359616
Adam Tang <adamtang@gmail.com>
2024-01-11 22:07:25 -0800

    removed like toggle button from My Likes

--- react-app/src/components/ShowcaseSongs.js
+++ react-app/src/components/ShowcaseSongs.js
@@ -1,12 +1,13 @@
 import SongTile from './Tile/SongTile';
 
-const ShowcaseSongs = ({ songs, h3, setShowModal }) => {
+const ShowcaseSongs = ({ songs, h3, setShowModal, likeDisabled=false }) => {
   return (
     <>
       <h3>{h3}</h3>
       <div className="showcase-grid flex-row">
         {songs?.map(song => (
           <SongTile
+            likeDisabled={likeDisabled}
             song={song}
             setShowModal={setShowModal}
             key={song?.id}
