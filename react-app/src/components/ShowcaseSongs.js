43aacab265c08c8bb5e4dbc6d9537c1b7d2a446c
Adam Tang <adamtang@gmail.com>
2024-04-19 23:14:02 -0700

    ensured ShowcaseSongs doesn't load until getAllSongs completes

--- react-app/src/components/ShowcaseSongs.js
+++ react-app/src/components/ShowcaseSongs.js
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllSongs } from '../store/song';
 import SongTile from './Tile/SongTile';
 
 const ShowcaseSongs = ({ songs, h3, setShowModal, likeDisabled=false }) => {
   const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     (async () => {
       await dispatch(getAllSongs());
+      setLoaded(true);
     })();
   }, [dispatch]);
 
+  if (!loaded) return null;
+  
   return (
     <>
       <h3>{h3}</h3>
