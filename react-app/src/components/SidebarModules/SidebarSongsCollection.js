d04bb9a21dbb13c1a960e810676248e9842d54ab
Adam Tang <adamtang@gmail.com>
2024-06-19 15:02:17 -0700

    implemented global switch for sidebar dropdowns using state

--- react-app/src/components/SidebarModules/SidebarSongsCollection.js
+++ react-app/src/components/SidebarModules/SidebarSongsCollection.js
@@ -1,95 +1,11 @@
 import { Link } from "react-router-dom";
 import SidebarCollection from "./SidebarCollection";
 import AssetCard from "../Modules/AssetCard";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AudioContext } from "../../context/AudioContext";
 import { useDispatch, useSelector } from "react-redux";
-import { loadSong, queueSong } from "../../store/player";
+import { loadSong } from "../../store/player";
 import { FaCirclePause, FaCirclePlay } from "react-icons/fa6";
-import { toggleSongLike } from "../../store/song";
-import ToggleButton from "../Buttons/ToggleButton";
-import CopyLinkButton from "../Buttons/CopyLinkButton";
-import DropdownButton from "../Buttons/DropdownButton";
-import { AnyDropdownContext } from "../../context/AnyDropdownContext";
-
-const SidebarSongButtonGroup = ({ song }) => {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const player = useSelector(state => state.player);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const baseClasses = ['cursor-pointer', 'composite-button'];
-  const styleClasses = ['button-action', 'b3'];
-
-  useEffect(() => {
-    if (!showDropdown) return;
-
-    const closeDropdown = () => {
-      if (!showDropdown) return;
-      setShowDropdown(false);
-    };
-
-    document.addEventListener("click", closeDropdown);
-    
-    return () => document.removeEventListener("click", closeDropdown);
-  }, [showDropdown]);
-
-  const handleSongLikeToggle = async (e) => {
-    e.stopPropagation();
-
-    const formData = new FormData();
-    formData.append("user_id", sessionUser?.id);
-    await dispatch(toggleSongLike(song?.id, formData));
-  };
-
-  const addSongToQueue = async (id) => {
-    if (!player?.currSongId) {
-      await dispatch(loadSong(id));
-    } else {
-      await dispatch(queueSong(id));
-    }
-  };
-
-  const dropdownItems = [
-    {
-      label: <CopyLinkButton
-        label="Copy Link"
-        link={`${window.location.origin}/songs/${song?.id}`}
-        isOfDropdown={true}
-      />,
-    },
-    {
-      onClick: () => addSongToQueue(song.id),
-      label: <div className="logo-before flex-row enqueue-label">
-        Add to queue
-      </div>,
-    },
-  ];
-
-  return (
-    <div className="mini-asset-button-group flex-row">
-      {sessionUser?.id !== song?.user?.id && (
-        <ToggleButton
-          condition={sessionUser?.id in song?.likes}
-          buttonClasses={[...baseClasses, 'b3']}
-          labelClasses={['heart-label']}
-          handleToggle={handleSongLikeToggle}
-          onLabel=""
-          offLabel=""
-        />
-      )}
-      <DropdownButton
-        toggleLabel=""
-        toggleClasses={styleClasses}
-        beforeLabel="ellipses-label"
-        labelSize="l3"
-        showDropdown={showDropdown}
-        setShowDropdown={setShowDropdown}
-        dropdownUlClasses={['menu', 'button-group-menu', 'flex-column']}
-        dropdownItems={dropdownItems}
-      />
-    </div>
-  );
-};
 
 const SidebarSongsCollection = ({
   collectionLink,
@@ -98,7 +14,6 @@ const SidebarSongsCollection = ({
   songs,
 }) => {
   const { play, pause, isPlaying } = useContext(AudioContext);
-  const { setShowAnyDropdown } = useContext(AnyDropdownContext);
   const dispatch = useDispatch();
   const player = useSelector(state => state.player);
 
@@ -116,7 +31,6 @@ const SidebarSongsCollection = ({
 
   return (
     <SidebarCollection
-      onMouseOut={() => setShowAnyDropdown(false)}
       collectionLink={collectionLink}
       styleClasses={styleClasses}
       h3={h3}
@@ -165,7 +79,6 @@ const SidebarSongsCollection = ({
               ) : (
                 <FaCirclePlay />
               )}
-              hoverButtonGroup={<SidebarSongButtonGroup song={song} />}
             />
           ))}
         </ul>
